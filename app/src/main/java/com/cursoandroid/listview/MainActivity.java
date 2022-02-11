8304cbf72fb400f51e0910e9f9fb34781d23652c
Tiago Pereira Barreto <93627992+TiagoPBarreto@users.noreply.github.com>
2022-02-11 11:54:12 -0300

    ListView

---
+++ app/src/main/java/com/cursoandroid/listview/MainActivity.java
@@ -0,0 +1,50 @@
+package com.cursoandroid.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listLocais;
+    private String[] itens = {
+            "Angra dos reis", "Caldas novas",
+            "Campos do jordão", "Costado sauipe",
+            "Ilheus", "Porto seguro", "Tiradentes",
+            "Praga", "Santiago", "Zurique", "Caribe",
+            "Buenos aires", "Budapeste", "Cancun","Aruba"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listLocais = findViewById(R.id.listLocais);
+        //adaptador para a lista
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                itens
+        );
+
+        //Adicionando adaptador na lista
+        listLocais.setAdapter(adaptador);
+
+        //Adicionar clique na lista
+        listLocais.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String valorSelecionado = listLocais.getItemAtPosition(i).toString();
+                Toast.makeText(getApplicationContext(),valorSelecionado, Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
